7d13f37bdbcefecaf7aef0e8bc0f810b47d08246
Frankenstei <enwereloveday@yahoo.com>
2022-11-22 09:21:58 -0800

    fifth commit

--- functions.c
+++ functions.c
@@ -94,7 +94,7 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	return (write(1, "%%", 1));
+	return (write(1, "%", 1));
 }
 
 /************************* PRINT INT *************************/
